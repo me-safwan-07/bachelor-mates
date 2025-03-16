9670347f4d40d747a22c8dc260efc064ecb2d179
me-safwan-07 <mesafwan07@gmail.com>
2025-03-16 11:39:04 +0530

    refactor Prisma client singleton; enhance database connection logging and improve environment handling

--- prisma/index.ts
+++ prisma/index.ts
@@ -2,12 +2,15 @@ import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
+  const client = new PrismaClient({
     datasources: { db: { url: process.env.DATABASE_URL } },
     ...(process.env.DEBUG === "1" && {
       log: ["query", "info"],
     }),
   }).$extends(withAccelerate());
+
+  console.log("Database connected");
+  return client;
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
@@ -18,4 +21,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
