5d78cc7b910a9768b95a5d444ec2b2f9e60b8dc7
me-safwan-07 <mesafwan07@gmail.com>
2025-03-24 07:21:37 +0530

    update @prisma/client version to 5.15.1; adjust node engine requirement and remove optional TypeScript peer dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,21 @@
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" translate="no">
+    <html lang="en" translate="no" suppressHydrationWarning>
       {/* <SpeedInsights sampleRate={0.1} /> */}
-      <body className="flex h-dvh flex-col transition-all ease-in-out">
+      <body className="flex h-dvh flex-col transition-all ease-in-out" suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
+          storageKey="theme-preference"
         >
           {children}
         </ThemeProvider>
