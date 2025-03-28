c0761183bc6d19ac5c9f92c6d8549eaed9b6e5dd
me-safwan-07 <mesafwan07@gmail.com>
2025-03-29 01:15:28 +0530

    fix: update Tailwind CSS plugins import syntax and upgrade TypeScript and Node.js type definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -110,5 +110,5 @@ module.exports = {
   },
   safelist: [{ pattern: /max-w-./, variants: "sm" }],
   darkMode: "class", // Set dark mode to use the 'class' strategy
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [import("@tailwindcss/forms"), import("@tailwindcss/typography")],
 }
