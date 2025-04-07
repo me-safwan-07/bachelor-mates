933f25d219645117ca453d457437dd88527a5633
me-safwan-07 <mesafwan07@gmail.com>
2025-04-07 23:38:39 +0530

    fix: adjust padding and margin in Footer component for better layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import { BookOpen } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="border-t bg-background">
-      <div className="container py-8 md:py-12">
+      <div className="container py-8 sm:px-4  md:py-12">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
           <div className="flex flex-col gap-2">
             <Link href="/" className="flex items-center gap-2">
